354164a37d44dcc220a08de38bdb9c96f33ec48f
ivan <ivankvkharkiv@gmail.com>
2021-10-12 17:08:04 +0300

    initial commit

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Recipient\Recipient;
+
+class NotificationController extends AbstractController
+{
+
+    /**
+     * @Route("/invoice/create")
+     */
+    public function create(NotifierInterface $notifier) : Response
+    {
+        // ...
+
+        // Create a Notification that has to be sent
+        // using the "email" channel
+        $notification = (new Notification('New Invoice', ['email']))
+            ->content('You got a new invoice for 15 EUR.');
+
+        // The receiver of the Notification
+        $recipient = new Recipient(
+            "testmail@testmail.com"
+        );
+
+        // Send the notification to the recipient
+        $notifier->send($notification, $recipient);
+
+        // ...
+
+
+
+        return new Response(
+            '<html><body>First Response</body></html>'
+        );
+
+    }
+
+}
